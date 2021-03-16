81f38e5a4b40be1d145e7d035d3610239bf4892d
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-17 01:23:47 +0545

    changed functional com to class com

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import React from 'react';
 import Checkbox from './components/Checkbox'
 import TodosData from './components/todosData'
-function App() {
-  const todoItems = TodosData.map(item => <Checkbox key={item.id} item={item}/>)
-  return (
-    <div className="todo-list">
-        {todoItems}
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      todos: TodosData
+    }
+  }
+  render(){
+    const todoItems = this.state.todos.map(item => <Checkbox key={item.id} item={item}/>)
+    return (
+      <div className="todo-list">
+          {todoItems}
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
