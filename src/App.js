5dab25576769d39105ba89dc4be46c9511cec194
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-17 16:09:33 +0545

    handelchange added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import './App.css';
 import React from 'react';
-import Checkbox from './components/Checkbox'
-import TodosData from './components/todosData'
+import TodoItem from './TodosItem'
+import todosData from './todosData'
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      todos: TodosData
-    }
+  constructor() {
+      super()
+      this.state = {
+          todos: todosData
+      }
+      this.handleChange = this.handleChange.bind(this)
   }
-  render(){
-    const todoItems = this.state.todos.map(item => <Checkbox key={item.id} item={item}/>)
-    return (
-      <div className="todo-list">
-          {todoItems}
-      </div>
-    );
+  
+  handleChange(id) {
+      this.setState(prevState => {
+          const updatedTodos = prevState.todos.map(todo => {
+              if (todo.id === id) {
+                return{
+                  ...todo,
+                  completed: !todo.completed
+                }
+              }
+              return todo
+          })
+          return {
+              todos: updatedTodos
+          }
+      })
   }
- 
+  
+  render() {
+      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+      
+      return (
+          <div className="todo-list">
+              {todoItems}
+          </div>
+      )    
+  }
 }
 
-export default App;
+export default App
